28ac1ec40e00f47d0001696232e286ecb96a4c83
kodurusupriya <supriyanaidu113@gmail.com>
2026-02-09 17:48:09 -0500

    actions

--- variables-date.sh
+++ variables-date.sh
@@ -6,7 +6,7 @@ echo "script executed at: $start_time"
 
 sleep 10
 
-end_time=$(date date+%s)
+end_time=$(date date +%s)
 
 total_time=$(($end_time-$start_time))
 echo "script executed in: $total_time"
