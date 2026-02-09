6629f9c40218ad113ea45b773a5d4c454511cc52
kodurusupriya <supriyanaidu113@gmail.com>
2026-02-09 18:05:44 -0500

    actions

--- special-variables.sh
+++ special-variables.sh
@@ -3,9 +3,9 @@
 echo "ALL args passed to script: $@"
 echo "Number of vars passed to script: $#"
 echo "script name: $0"
-echo "present directory: $pwd"
-echo "who is running: $user"
-echo "home directory of current user: $home"
+echo "present directory: $PWD"
+echo "who is running: $USER"
+echo "home directory of current user: $HOME"
 echo "pid of this script: $$"
 sleep 100 &
 echo "background process id: $!"
