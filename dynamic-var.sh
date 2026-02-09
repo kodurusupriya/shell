9d2ee8d981d743a537565933d35e362405a0ef87
kodurusupriya <supriyanaidu113@gmail.com>
2026-02-08 22:09:51 -0500

    actions

---
+++ dynamic-var.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+p1=$1
+p2=$2
+echo "$p1:: Hello $p2, How are you?"
+echo "$p2:: Hi $p1, I am good , How you been?"
+echo "$p1:: How is Job life going on?"
+echo "$p2:: It's wonderful had a chance to learn more"
