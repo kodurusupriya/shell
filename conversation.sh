376cc9b55cbe2b1da56c5dc229c63aacc8b88ce5
kodurusupriya <supriyanaidu113@gmail.com>
2026-02-08 21:51:32 -0500

    conversation

---
+++ conversation.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "vini:: Hello supri, How are you?"
+echo "supi:: Hi Vini, I am good , How you been?"
+echo "Vini:: How is Job life going on?"
+echo "supi:: It's wonderful had a chance to learn more"
