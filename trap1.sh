4d74de4a0d91abd5599f8b2dfe4b36f5353ca144
kodurusupriya <supriyanaidu113@gmail.com>
2026-02-09 23:18:28 -0500

    actions

---
+++ trap1.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+set -e 
+trap 'echo "There is an error in $LINEEND, command: $BASH-COMMAND"' ERR
+USERID=$(id -u)
+LOGS_FOLDER="/var/log/shell"
+LOGS_FILE="/var/log/shell/$0.log"
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[34m"
+ 
+if [ $USERID -ne 0 ]; then
+    echo -e "$R please run this script with root user access $N"  | tee -a $LOGS_FILE
+    exit 1
+fi
+
+mkdir -p $LOGS_FOLDER
+
+
+
+ for package in $@ #sudo sh.loops-functions.sh nginx mysql nodejs
+ do 
+    dnf list installed $package &>>$LOGS_FILE 
+    if [ $? -ne 0 ]; then
+            echo "$package not installed, installing now"
+            dnf install $package -y &>>$LOGS_FILE
+            
+    else
+           echo -e "$package already installed ... $Y skipping $N"
+     fi              
+done
