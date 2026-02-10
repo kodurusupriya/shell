a18d5ff65da6dbfee402ed652d7ed6d02de3607e
kodurusupriya <supriyanaidu113@gmail.com>
2026-02-09 20:56:00 -0500

    actions

--- functions.sh
+++ functions.sh
@@ -1,33 +1,34 @@
 #!/bin/bash
 
 USERID=$(id -u)
+LOGS_FOLDER="/var/log/shell"
+LOGS_FILE="/var/log/shell/$0.log"
  
 if [ $USERID -ne 0 ]; then
     echo "please run this script with root user access"
     exit 1
 fi
+
+mkdir -p $LOGS_FOLDER
+
 #by default shell will not execute,only executed when called
 VALIDATE(){
 
-if [ $1 -ne 0 ]; then 
-    echo "$2 ... failure"
-    exit 1
-else 
-    echo "$2 ... success"   
-fi     
+        if [ $1 -ne 0 ]; then 
+            echo "$2 ... failure"
+            exit 1
+        else 
+            echo "$2 ... success"   
+        fi     
 
 }
 
 
-dnf install nginx -y
-
+dnf install nginx -y &>> $LOGS_FILE
 VALIDATE $? "nginx installation" 
  
-
-dnf install mysql -y 
-
+dnf install mysql -y &>> $LOGS_FILE
 VALIDATE $? "mysql installation"   
 
-dnf install nodejs -y
-
+dnf install nodejs -y &>> $LOGS_FILE
 VALIDATE $? "nodejs installation"   
